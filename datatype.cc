e642fa540bbdf683901b0d99d4c208813dd0ddb8
Gulafshan Parveen <64768393+GulafshanParveen@users.noreply.github.com>
2020-06-25 23:48:19 +0530

    Datatype declaration

---
+++ datatype.cc
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+int main() {
+        int a;
+        long e;
+        char b;
+        float c;
+        double d;
+   cin>> a >> e >>b >> c >>d;
+        cout<<a<<endl;
+        cout<<e<<endl;
+        cout<<b<<endl;
+        cout<<fixed<<setprecision(3)<<d<<"\n";//setprecision is used for fixed decimal like C (.2f\n)
+        cout<<fixed<<setprecision(9)<<e<<"\n";
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+        return 0;
+}
+
